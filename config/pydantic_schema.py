526566b1fbf4c8870d1164a301c7c24784f35143
aufflammen <178506932+aufflammen@users.noreply.github.com>
2025-03-10 14:19:48 +0300

    add validation script and prompts update

--- config/pydantic_schema.py
+++ config/pydantic_schema.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel, Field, ConfigDict
 from enum import Enum
 
+
 class ContainerNameEnum(str, Enum):
     """Типы контейнеров"""
-    dc = "DC" # Dry Container
-    nc = "HC" # "High Cubes"
-    ot = "OT" # "Open Top"
-    pw = "PW" # "Pallet Wide"
-    dd = "DD" # "Double Doors"
-    gp = "GP" # " General Purpose"
+    dc = "DC"  # Dry Container
+    nc = "HC"  # "High Cubes"
+    ot = "OT"  # "Open Top"
+    pw = "PW"  # "Pallet Wide"
+    dd = "DD"  # "Double Doors"
+    gp = "GP"  # " General Purpose"
     tk = "TK"
     tc = "TC"
     dv = "DV"
@@ -16,6 +17,7 @@ class ContainerNameEnum(str, Enum):
     fh = "RH"
     vgp = "VGP"
     ft = "FT"
+    hq = "HQ"
     h = "H"
     bulk = "Bulk"
 
@@ -32,9 +34,8 @@ class ContainersSchema(BaseModel):
     container_goods: str = Field(description=(
         "Detailed description of the goods inside the container. "
         "This information is commonly found under sections labeled 'Description of Goods', 'Cargo Description', or 'Goods'. "
-        "It may include product names, material types, HS codes (Harmonized System), and packaging details. "
-        "Exclude any references to container size, type, or format (e.g., '1X40' HC', 'FCL', 'STC'). "
-        "Extract only the actual cargo details. "
+        "**Include only relevant details such as product names, material types, and packaging information.** "
+        "**Exclude any unrelated information**, such as container type, container size, contact details (e.g., email, phone number), tax identification numbers (e.g., INN), or addresses. "
         "If multiple products are listed, return all relevant items."
     ))
     seals: list[str] = Field(description=(
@@ -46,18 +47,20 @@ class ContainersSchema(BaseModel):
         "In such cases, extract only the seal number(s) and exclude container number, size, and other values like weights or product references. "
         "If multiple seals exist, return all of them in the order they appear."
     ))
-    size: int = Field(description=(
+    size: int | None = Field(description=(
         "Container size in feet, typically 20, 40, or 45. "
         "If found in a structured format like 'SIZE_TYPE/CONTAINER/SEAL/OTHER_DATA', extract only the numerical size value."
     ))
     type: ContainerNameEnum = Field(description=(
         "Standardized container type code (e.g., 'DC' for Dry Container, 'HC' for High Cube). "
-        "If found in a structured format such as 'SIZE_TYPE/CONTAINER/SEAL/OTHER_DATA', extract only the container type."
+        "If found in a structured format such as 'SIZE_TYPE/CONTAINER/SEAL/OTHER_DATA', extract only the container type. "
+        "If not found, return an empty string ('')"
     ))
     gross_weight: float | None = Field(description=(
         "Total weight of the container including goods, in kilograms (kg). "
         "Often labeled as 'Gross Weight', 'GW', 'Weight', or 'WT'. "
-        "Ensure the unit is in kilograms and convert if necessary."
+        "Ensure the unit is in kilograms and convert if necessary. "
+        "If only a single Gross Weight value is provided and there are multiple containers listed in the document, divide the Gross Weight equally among the number of containers."
     ))
     tare_weight: float | None = Field(description=(
         "Tare weight of the container (weight of the empty container) in kilograms (kg). "
@@ -93,12 +96,15 @@ class CompanyWithNumbersSchema(BaseCompanySchema):
     """
     inn: int | None = Field(description=(
         "Taxpayer Identification Number (ИНН) of the company, if available. "
-        "Extract strictly as a 10- or 12-digit numeric value."
+        "Extract strictly as a 10- or 12-digit numeric value. "
+        "Make sure the number is associated with the company name in the context."
     ))
     kpp: int | None = Field(description=(
         "Tax Registration Reason Code (КПП) of the company, if available. "
-        "Extract strictly as a 9-digit numeric value."
+        "Extract strictly as a 9-digit numeric value. "
+        "Make sure the number is associated with the company name in the context."
     ))
+    # TODO: доработать промпт, inn consignee попадает в inn notify
 
 
 class PydanticSchema(BaseModel):
@@ -131,7 +137,7 @@ class PydanticSchema(BaseModel):
     ))
     voyage: str = Field(description=(
         "Voyage number assigned to the vessel for this shipment. "
-        "Common labels include 'Voyage No.', 'Voy. No.', or 'Voyage'."
+        "Common labels include 'Voyage No.', 'Voy. No.', 'V#', or 'Voyage'."
     ))
     port_of_loading: str = Field(description="Port where the cargo is loaded onto the vessel")
     port_of_discharge: str = Field(description="Port where the cargo is discharged from the vessel.")
